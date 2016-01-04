092356e89342bb767c266ef33f399fc6dec8c5ec
Mopkau <info@gromoplan.com.ua>
2016-01-04 20:31:20 +0200

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="text/javascript" src="keyboard.js" charset="UTF-8"></script>
+    <link rel="stylesheet" type="text/css" href="keyboard.css">
+</head>
+<body>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+<input type="text"><br>
+</body>
